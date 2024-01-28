d1e1c82813b77644a72e35b09018c2d9543d5c98
Caio Carauta <caiocarauta3@gmail.com>
2024-01-28 16:58:52 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+
+    elementoPlataformas.classList.toggle("ativo");
+
+
+});
+
+
